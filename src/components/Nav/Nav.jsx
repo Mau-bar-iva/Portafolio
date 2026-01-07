c4bbb2c19ded02842dff05378b0a5b79e8a4083a
Mauricio <zub_gg@hotmail.com>
2026-01-07 20:22:19 -0300

    nav-mobile-text-try.4

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -9,12 +9,12 @@ export default function Nav({ mobile }) {
     return (
         <nav className={`${mobile && "w-full flex items-center justify-end"}`}>
             {mobile ? (
-                <div className={` ${menuVisible ? "nav-mobile-btn-container open" : "nav-mobile-btn-container closed"} h-fit  relative px-[25px] pt-[3rem] pb-[1rem] rounded-full bg-[#0E1452]  flex flex-col items-center justify-center`} >
+                <div className={` ${menuVisible ? "nav-mobile-btn-container open" : "nav-mobile-btn-container closed"} h-fit  relative px-[25px] pt-[3rem] pb-[1rem] rounded-full bg-[#0E1452]  flex flex-col items-center justify-center z-[99]`} >
                     <button className="hamburger-btn self-end" onClick={toggleMenu}>
                         <Menu size={30} color="#e8fd1c" />
                     </button>
-                    {!menuVisible && (
-                        <ul className="nav-mobile-menu absolute w-full  left-0 flex flex-col items-center text-2xl text-[#2076e0] gap-8">
+                    {menuVisible && (
+                        <ul className="nav-mobile-menu absolute w-full mt-[20rem] left-0 flex flex-col items-center text-2xl text-[#2076e0] gap-8 z-[999]">
                             <li className="hover:text-[#dfc48d]"><a href="#section-inicio">Inicio</a></li>
                             <li className="hover:text-[#dfc48d]"><a href="#section-aboutme">Sobre mi</a></li>
                             <li className="hover:text-[#dfc48d]"><a href="#section-proyects">Proyectos</a></li>
